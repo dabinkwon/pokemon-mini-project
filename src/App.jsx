e39918e165c1ef216ae116d5fd350c8a5af7021e
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-09-01 21:06:30 +0900

    fix: 로딩 처리

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { lazy, Suspense, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchMultiplePokemonById } from "./RTK/thunk";
 import { Link, Route, Routes, useNavigate } from "react-router-dom";
-const Main = lazy(()=>import( "./pages/Main"))
-const Detail = lazy(()=>import( "./pages/Detail"))
-const Search = lazy(()=>import( "./pages/Search"))
-const Favorite = lazy(()=>import( "./pages/Favorite"))
-const NotFound = lazy(()=>import("./pages/NotFound"))
+const Main = lazy(() => import("./pages/Main"));
+const Detail = lazy(() => import("./pages/Detail"));
+const Search = lazy(() => import("./pages/Search"));
+const Favorite = lazy(() => import("./pages/Favorite"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   const dispatch = useDispatch();
@@ -34,7 +34,7 @@ function App() {
               className="border-2 rounded-lg pl-2 p-1 w-[189px] text-[14px] outline-0"
               placeholder="포켓몬 이름을 입력하세요."
               onChange={(e) => {
-                navigate(`/search?pokemon=${e.target.value}`)
+                navigate(`/search?pokemon=${e.target.value}`);
               }}
               type="text"
             />
@@ -42,13 +42,19 @@ function App() {
         </nav>
       </header>
       <main className="flex justify-center flex-wrap gap-9 items-center mt-8">
-        <Suspense fallback={<div>로딩중...</div>}>
+        <Suspense
+          fallback={
+            <div className="flex justify-center items-center h-82">
+              <div className="w-14 h-14 border-4 border-blue-500 rounded-full border-t-transparent animate-spin"></div>
+            </div>
+          }
+        >
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/detail/:pokemonId" element={<Detail />} />
             <Route path="/search" element={<Search />} />
             <Route path="/favorite" element={<Favorite />} />
-            <Route path="*" element={<NotFound/>}/>
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Suspense>
       </main>
